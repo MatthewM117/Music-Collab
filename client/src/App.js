5e23f041b7bdf18c68458fe951047d506575c6e6
matthewm588 <matthewj.morelli@gmail.com>
2023-05-15 13:54:28 -0400

    added moving bar along the piano roll

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import './styles.css';
 import io from 'socket.io-client';
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useRef} from 'react';
 
 const socket = io.connect("http://localhost:3001")
 
@@ -10,6 +10,12 @@ function App() {
   const [message, setMessage] = useState('');
   const [messageReceived, setMessageReceived] = useState('');
   const [gridData, setGridData] = useState(Array.from({ length: 50 }, () => Array.from({ length: 100 }, () => "")));
+  const [isBarMoving, setBarMoving] = useState(false);
+  const [bpm, setBpm] = useState(120);
+  const beatsPerMeasure = 4; // Number of beats required to traverse 4 cells
+  const secondsPerBeat = 60 / bpm;
+  const cellSize = 2000;
+  const animationDuration = (beatsPerMeasure * secondsPerBeat * 1000 * cellSize) / 100;
 
   const joinRoom = () => {
     if (room !== '') {
@@ -75,6 +81,83 @@ function App() {
    * to edit the number of rows, simply change the first length value where const gridData is declared
    */
   
+  const barRef = useRef(null)
+
+  function toggleBarAnimation() {
+    setBarMoving((prevState) => !prevState);
+  }
+
+  useEffect(() => {
+    if (isBarMoving) {
+      barRef.current.style.animation = `moveBar ${animationDuration / 1000}s linear infinite`;
+    } else {
+      barRef.current.style.animation = '';
+    }
+  }, [isBarMoving, animationDuration]);
+
+  // keyboard input
+  const handleKeyDown = (event) => {
+    if (event.code === 'Space') {
+      event.preventDefault();
+      toggleBarAnimation();
+    }
+  };
+
+  const handleAnimInputChange = (event) => {
+    setBpm(Number(event.target.value));
+  }
+
+  let debounce = false;
+  let prevI = -1;
+  const checkCellBackgroundColor = () => {
+    const barPosition = barRef.current.getBoundingClientRect();
+    const cells = document.getElementsByClassName('cell--active');
+
+    for (let i = 0; i < cells.length; i++) {
+      const cellPosition = cells[i].getBoundingClientRect();
+
+      if (
+        barPosition.left <= cellPosition.right &&
+        barPosition.right >= cellPosition.left &&
+        barPosition.top <= cellPosition.bottom &&
+        barPosition.bottom >= cellPosition.top
+      ) {
+        const cellColor = window.getComputedStyle(cells[i]).backgroundColor;
+
+        if (!debounce) {
+          console.log('touched a note');
+          if (cellColor === 'rgb(255, 0, 0)') {
+            console.log("RED!");
+          }
+          debounce = true;
+          prevI = i;
+        }
+
+        if (prevI !== i) {
+          debounce = false;
+        }
+      }
+      //console.log(prevI, " ", i);
+    }
+
+    if (isBarMoving) {
+      requestAnimationFrame(checkCellBackgroundColor);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []); // empty dependecy array in order to ensure this is only ran once
+
+  useEffect(() => {
+    if (isBarMoving) {
+      requestAnimationFrame(checkCellBackgroundColor);
+    }
+  }, [isBarMoving]);
+
   return (
     <div className="App">
       <input 
@@ -91,8 +174,12 @@ function App() {
       <h1> Message: </h1>
       {messageReceived}
 
+      <label htmlFor="bpmInput">BPM:</label>
+      <input id="bpmInput" type="number" value={bpm} onChange={handleAnimInputChange} />
+
       <div className="grid-container">
         <div className="grid" style={{ height: "100vh", width: "500vw" }}>
+        <div ref={barRef} className="bar"></div>
         {gridData.map((row, rowIndex) => (
           <div key={rowIndex} className="row">
             {Array.from(row).slice(0, numCols).map((cell, cellIndex) => (
@@ -109,6 +196,7 @@ function App() {
           </div>
         ))}
       </div>
+
     </div>
 
     </div>
